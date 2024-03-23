baa03712fddd9ff6fc384ee210f4205dd8762402
杨培琨 <745653843@qq.com>
2024-03-23 21:05:58 +0800

    add server

---
+++ modules/gui/src/module/server/TcpServerDialog.cpp
@@ -0,0 +1,50 @@
+﻿#include "TakeLeaveDialog.h"
+#include "ui_TakeLeaveDialog.h"
+#include "src/module/common/OperationCheckDialog.h"
+#include <QtConcurrent/qtconcurrentrun.h>
+#include <QFuture>
+#include <Qtimer>
+#include <QStandardItemModel>
+#include <QtWidgets>
+
+
+//#include "signal_transmitter.h"
+//#include "as/IniPara.hpp"
+
+TakeLeaveDialog::TakeLeaveDialog(QWidget *parent)
+    : ASQDialog(parent),
+	m_ui(new Ui::TakeLeaveDialog)
+{
+	m_ui->setupUi(this);
+
+    InitUI();
+    InitConnect();
+}
+
+TakeLeaveDialog::~TakeLeaveDialog()
+{
+	delete m_ui;
+}
+
+void TakeLeaveDialog::InitUI()
+{
+
+
+
+}
+
+void TakeLeaveDialog::InitConnect()
+{
+    
+    connect(m_ui->toolButton_look, static_cast<void (QToolButton::*)(bool)>(&QToolButton::clicked), this, [this](bool checked)
+        {
+            Q_UNUSED(checked);
+            m_cSubmitDialog = new SubmitDialog(this);
+            m_cSubmitDialog->setGeometry(this->geometry().x(), this->geometry().y(), this->geometry().width(), this->geometry().height());
+            if (m_cSubmitDialog->exec() == QDialog::Accepted)
+            {
+                
+            }
+        });
+}
+
---
+++ modules/gui/src/module/server/TcpServerDialog.h
@@ -0,0 +1,45 @@
+﻿#ifndef AS_TAKELEAVEDIALOG_H
+#define AS_TAKELEAVEDIALOG_H
+
+#include "common/CommonPart.h"
+#include "as/CommonCore.hpp"
+#include "as/common/UiCallback.h"
+
+#include "SubmitDialog.h"
+
+//
+#include "as/common/CommonProcess.h"
+
+//Qt
+#include <QLineEdit>
+
+namespace Ui
+{
+	class TakeLeaveDialog;
+}
+using namespace as;
+
+class TakeLeaveDialog : public ASQDialog
+{
+    Q_OBJECT
+public:
+	TakeLeaveDialog(QWidget* parent = nullptr);
+	~TakeLeaveDialog();
+
+	void InitUI();
+	
+	void InitConnect();
+	
+signals:
+
+private slots:
+
+private:
+    Ui::TakeLeaveDialog *m_ui;
+
+	SubmitDialog* m_cSubmitDialog;
+	
+
+};
+
+#endif
